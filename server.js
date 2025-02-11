c7748f407f61c91b04c376381550e485f2f781f1
Ramakrishnajakkula <ramanjaneyuluj14@gmail.com>
2025-02-11 15:52:01 +0530

    g

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ["https:/localhost:3000"],
+  origin: ["http://localhost:5173"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
