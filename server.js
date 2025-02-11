54787fa2d8a26fa268f506a4fec91fd94e831f32
Ramakrishnajakkula <ramanjaneyuluj14@gmail.com>
2025-02-11 16:01:56 +0530

    g

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ["http://localhost:5173","https://ramakrishnajakkula.github.io/SPC/"],
+  origin: ["http://localhost:5173","https://ramakrishnajakkula.github.io"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
