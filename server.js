6768bdbe65742c58e8c0741e55ec98444e0ceee9
Ramakrishnajakkula <ramanjaneyuluj14@gmail.com>
2025-02-11 09:01:38 +0530

    hh

--- server.js
+++ server.js
@@ -6,17 +6,11 @@ const auth = require('../middleware/auth');
 
 // Test endpoint
 router.get('/test', (req, res) => {
-  try {
-    res.status(200).json({ 
-      message: 'Auth endpoint is working!',
-      timestamp: new Date().toISOString()
-    });
-  } catch (error) {
+  
     res.status(500).json({ 
       message: 'Auth endpoint error',
       error: error.message 
     });
-  }
 });
 
 router.post('/signup', authController.signup);
