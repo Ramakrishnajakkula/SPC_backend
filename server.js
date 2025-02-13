fece9e42daeb15a72c5ca7c23116492cc73b6176
Ramakrishnajakkula <ramanjaneyuluj14@gmail.com>
2025-02-13 19:30:18 +0530

    added local host

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ["http://localhost:5174","https://ramakrishnajakkula.github.io"],
+  origin: ["http://localhost:5174","http://localhost:5174","http://localhost:3000","https://ramakrishnajakkula.github.io"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
